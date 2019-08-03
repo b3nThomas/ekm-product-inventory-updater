063809ddc7fc96256e2963d9823b831a2456ef2f
b3nThomas <b3nadthomas@gmail.com>
2019-08-04 00:28:13 +0100

    Got it working good! 😎

--- app/EKMClient.ts
+++ app/EKMClient.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import * as colors from 'colors';
+import * as convert from 'xml-js';
+import * as DOMParser from 'dom-parser';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -10,6 +12,13 @@ export interface EKMAuth {
 
 export class EKMClient {
     private auth: EKMAuth;
+    private requestHeaders: any = {
+        headers: {
+            'Accept': 'text/xml',
+            'Content-Type': 'text/xml'
+        }
+    };
+    private parser: any = new DOMParser();
 
     constructor() {
         this.getAuth();
@@ -20,7 +29,6 @@ export class EKMClient {
         try {
             authFile = fs.readFileSync(path.resolve('app', 'auth.json'), { encoding: 'utf-8' });
             authFile = JSON.parse(authFile);
-            console.log(authFile);
         } catch (err) {
             console.log(colors.red('Unable to find ./app/auth.json. Please see README.md. Aborting...\n'));
             return process.exit(1);
@@ -31,37 +39,62 @@ export class EKMClient {
         }
         this.auth = {
             endpoint: authFile.endpoint,
-            key: authFile.endpoint
+            key: authFile.key
         }
+        console.log(colors.green('Authorization credentials retrieved 👍\n'));
     }
 
-    public async getProducStock(itemId: string): Promise<any> {
+    public async getProducStock(productCode: string): Promise<number> {
+        console.log(`Requesting product stock count for product code "${productCode}"...`);
         // http://publicapi.30.ekm.net/v1.1/publicapi.asmx?op=GetProductStock
-        const xmlTemplate = `
-            <?xml version="1.0" encoding="utf-8"?>
-            <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        let xmlTemplate: any = `
+            <?xml version="1.0" encoding="UTF-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
                 <soap:Body>
                     <GetProductStock xmlns="http://publicapi.ekmpowershop.com/">
                         <GetProductStockRequest>
                             <APIKey>${this.auth.key}</APIKey>
-                            <ProductCode>${itemId}</ProductCode>
+                            <ProductCode>${productCode}</ProductCode>
                         </GetProductStockRequest>
                     </GetProductStock>
                 </soap:Body>
             </soap:Envelope>
-        `;
+        `.replace(/\n/g, '').trim();
+        xmlTemplate = this.parser.parseFromString(xmlTemplate, 'text/xml').rawHTML;
         try {
-            const res = await axios.post(this.auth.endpoint, xmlTemplate);
-            console.log(res);
-            return res;
+            let res: any = await axios.post(this.auth.endpoint, xmlTemplate, this.requestHeaders);
+            res = convert.xml2js(res.data, { compact: true });
+            const count = parseInt(res['soap:Envelope']['soap:Body']['GetProductStockResponse']['GetProductStockResult']['ProductStock']['_text'], 10);
+            return count;
         } catch (err) {
-            console.log(colors.red('getStockCount request failed'), itemId);
-            console.error(err);
+            console.log(colors.red(`getStockCount request failed for item "${productCode}"\n`));
+            console.log(colors.red(err));
         }
     }
 
-    // public async setProductStock(itemId: string, stockCount: number) {
-    //     // http://publicapi.30.ekm.net/v1.1/publicapi.asmx?op=SetProductStock
-
-    // }
+    public async setProductStock(productCode: string, stockCount: number): Promise<any> {
+        // http://publicapi.30.ekm.net/v1.1/publicapi.asmx?op=SetProductStock
+        console.log(`Setting product stock count to "${stockCount}" for product code "${productCode}"...`);
+        let xmlTemplate: any = `
+            <?xml version="1.0" encoding="UTF-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+                <soap:Body>
+                    <SetProductStock xmlns="http://publicapi.ekmpowershop.com/">
+                        <SetProductStockRequest>
+                            <APIKey>${this.auth.key}</APIKey>
+                            <ProductCode>${productCode}</ProductCode>
+                            <ProductStock>${stockCount}</ProductStock>
+                        </SetProductStockRequest>
+                    </SetProductStock>
+                </soap:Body>
+            </soap:Envelope>
+        `.replace(/\n/g, '').trim();
+        xmlTemplate = this.parser.parseFromString(xmlTemplate, 'text/xml').rawHTML;
+        try {
+            let res: any = await axios.post(this.auth.endpoint, xmlTemplate, this.requestHeaders);
+            res = convert.xml2js(res.data, { compact: true });
+            return res;
+        } catch (err) {
+            console.log(colors.red(`setStockCount request failed for item "${productCode}"\n`));
+            console.log(colors.red(err));
+        }
+    }
 }
--- start.ts
+++ start.ts
@@ -1,4 +1,27 @@
+import * as colors from 'colors';
+import * as fs from 'fs';
+import * as path from 'path';
 import { EKMClient } from './app/EKMClient';
 
-new EKMClient();
+import * as csv from 'convert-csv-to-json'; // REPLACE
 
+const ekmClient = new EKMClient();
+
+(async () => {
+    let data;
+    try {
+        data = csv.fieldDelimiter(',').generateJsonFileFromCsv(path.resolve('app', 'data.csv'), path.resolve('app', 'data.json'));
+    } catch (err) {
+        console.log(colors.red('Unable to locate ./app/data.csv file. See README.md. Aborting...\n'));
+        process.exit(1);
+    }
+
+    const json = fs.readFileSync(path.resolve('app', 'data.json'), { encoding: 'utf-8' }); // tslint:disable-line
+
+    console.log(json);
+    const current = await ekmClient.getProducStock('SALTEA');
+    console.log(colors.green(`SALTEA: ${current} in stock`));
+    const sold = 4;
+    const newAmount = current + sold;
+    ekmClient.setProductStock('SALTEA', newAmount);
+})();
