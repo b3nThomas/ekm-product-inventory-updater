45ccc2da8e9cb98788aa671e9e8b1de22ca9558f
b3nThomas <b3nadthomas@gmail.com>
2019-08-04 11:43:12 +0100

    Update fields

--- app/start.ts
+++ app/start.ts
@@ -34,7 +34,7 @@ const ekmClient = new EKMClient();
                 promises.length = 0;
             }
 
-            if (!entry.ItemID || !entry.Quantity) {
+            if (!entry.ItemID || !entry.Stock) {
                 console.error(colors.red('✖︎ Invalid product entry found. Skipping...'));
                 console.error(colors.red(entry));
                 bar.tick();
